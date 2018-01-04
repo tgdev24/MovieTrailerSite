be672a4e9d592267e8b5fc5307b26f094801e1d3
tgdev24 <tgdev24@users.noreply.github.com>
2018-01-04 13:52:26 -0800

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,21 @@
+import fresh_tomatoes
+import movie
+
+# instantiation of movies by giving arguments 
+movie1 = movie.Movie('Avengers: Infinity War',
+                     'Movie about superheroes and one giant purple villain',
+                     'https://news.marvel.com/wp-content/uploads/2017/07/studios_post_master-960x540.jpg',
+                     'https://www.youtube.com/watch?v=6ZfuNTqbHE8')
+movie2 = movie.Movie('The New Mutants',
+                     'Movie about teenage X-men',
+                     'http://t2.gstatic.com/images?q=tbn:ANd9GcR3OXGa2wVRnoOiqcnpcpNuQJ7ue5C38JlqOkqnnTZvWa2UVVRk',                     
+                     'https://www.youtube.com/watch?v=bu9e410C__I')
+movie3 = movie.Movie('Ant Man and the Wasp',
+                     'Movie about guy that turns into an ant',
+                     'http://t2.gstatic.com/images?q=tbn:ANd9GcRxIQHv1T0L649pKR64-WOZpiWGKw5R7W7hkxK2Crl3EiQwZEja',
+                     'https://www.youtube.com/watch?v=XyZERzqh090')
+# put all movie objects created into a list
+movies = [movie1, movie2, movie3]
+
+fresh_tomatoes.open_movies_page(movies) # open_movies_page takes in a list of movie objects
+print(movie.Movie.__doc__)
---
+++ movie.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+class Movie():
+    """ Class that sets the title, storyline, poster image, and trailer url
+        to movie objects """
+    def __init__(self, movie_title, storyline, poster_image_url, trailer_url):
+        """ Constructor for the Movie class """
+        self.title = movie_title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_url
+    
+    def show_trailer(self):
+        """ Opens up the trailer at the designated url """
+        webbrowser.open(self.trailer_url)
+
+    
