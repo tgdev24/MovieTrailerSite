aede2528b3e49986467da0ce3cdfc60397bc52b1
Tsenddavaa Ganbat <tsenddavaa@sdfutures.org>
2018-01-19 17:15:46 -0800

    hello

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,23 +5,24 @@ import movie
 movie1 = movie.Movie(
         'Avengers: Infinity War',
         'Movie about superheroes and one giant purple villain',
-        'https://news.marvel.com/wp-content/uploads/2017/07/studios_post_master-960x540.jpg', # NOQA
+        'https://news.marvel.com/wp-content/uploads/2017/07/studios_post_master-960x540.jpg',      # noqa
         'https://www.youtube.com/watch?v=6ZfuNTqbHE8'
         )
 movie2 = movie.Movie(
         'The New Mutants',
         'Movie about teenage X-men',
-        'http://t2.gstatic.com/images?q=tbn:ANd9GcR3OXGa2wVRnoOiqcnpcpNuQJ7ue5C38JlqOkqnnTZvWa2UVVRk', #NOQA                     
+        'http://t2.gstatic.com/images?q=tbn:ANd9GcR3OXGa2wVRnoOiqcnpcpNuQJ7ue5C38JlqOkqnnTZvWa2UVVRk',      # noqa                    
         'https://www.youtube.com/watch?v=bu9e410C__I'
         )
 movie3 = movie.Movie(
         'Ant Man and the Wasp',
         'Movie about guy that turns into an ant',
-        'http://t2.gstatic.com/images?q=tbn:ANd9GcRxIQHv1T0L649pKR64-WOZpiWGKw5R7W7hkxK2Crl3EiQwZEja',  #NOQA
+        'http://t2.gstatic.com/images?q=tbn:ANd9GcRxIQHv1T0L649pKR64-WOZpiWGKw5R7W7hkxK2Crl3EiQwZEja',   # noqa
         'https://www.youtube.com/watch?v=XyZERzqh090'
         )
 # put all movie objects created into a list
 movies = [movie1, movie2, movie3]
 
-fresh_tomatoes.open_movies_page(movies) # open_movies_page takes in a list of movie objects
+# open_movies_page takes in a list of movie objects
+fresh_tomatoes.open_movies_page(movies) 
 print(movie.Movie.__doc__)
--- movie.py
+++ movie.py
@@ -1,6 +1,7 @@
 import webbrowser
 
-class Movie():
+
+class Movie():    
     """ Class that sets the title, storyline, poster image, and trailer url
         to movie objects """
     def __init__(self, movie_title, storyline, poster_image_url, trailer_url):
@@ -9,9 +10,7 @@ class Movie():
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_url
-    
+        
     def show_trailer(self):
         """ Opens up the trailer at the designated url """
         webbrowser.open(self.trailer_url)
-
-    
